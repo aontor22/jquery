7501d1e6c020e61a981c494431838502d6dc2d71
aontor22 <udoychowdhury90413@gmail.com>
2025-10-05 14:28:39 +0600

    lesson 14 & 15

---
+++ 05 JQuery-add-remove/js/app.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $('button').click(function(){
+//         // $('p').append("<b>Udoy</b>");   // Add after line
+        
+//         // $('p').prepend("<b>Udoy</b>");      // Add before line
+//         $("img").before(" Hello...........");     // before the image
+//         $("img").after(" Hello...........");     // after the image
+//     });
+// });
+
+// $(document).ready(function(){
+//     $('.btn').click(function(){
+//         // $('.udoy').remove();      // remove the entire div
+        
+//         $('.udoy').empty();      // remove all the of the div
+//     });
+// });
+
+$(document).ready(function(){
+    $('.btn1').click(function(){   // remove the specific element
+        $('.gg').remove();   
+    });
+});
+
